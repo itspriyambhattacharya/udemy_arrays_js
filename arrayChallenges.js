49ff487bd91ec4c48a365022b9f6e08acc3a37ab
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2024-10-18 19:15:57 +0530

    solved question 2 and done a little formatting

--- arrayChallenges.js
+++ arrayChallenges.js
@@ -5,20 +5,28 @@ console.clear();
 let teaFlavours = ["green tea", "black tea", "oolong tea"];
 let firstTea = teaFlavours[0];
 console.log(firstTea);
+console.log("-------------------------------------------------");
 
 //Question 2
+
+let cities = ["London", "Tokyo", "Paris", "New York"];
+let favouriteCity = cities[2];
+console.log(favouriteCity);
+console.log("-------------------------------------------------");
 
 // Question 3
 
 let teaTypes = ["herbal tea", "white tea", "masala tea"];
 teaTypes[1] = "jasmine tea";
 console.log(teaTypes);
+console.log("-------------------------------------------------");
 
 //Question 4
 
 let citiesVisited = ["Mumbai", "Sydney"];
 citiesVisited.push("Berlin");
 console.log(citiesVisited);
+console.log("-------------------------------------------------");
 
 //Question 5
 
@@ -26,6 +34,7 @@ let teaOrdes = ["chai", "iced tea", "matcha tea", "earl tea"];
 
 let lastOrder = teaOrdes.pop();
 console.log(lastOrder);
+console.log("-------------------------------------------------");
 
 //Question 6
 
@@ -33,6 +42,7 @@ let popularTeas = ["green tea", "oolong tea", "chai"];
 let softCopyTree = popularTeas;
 console.log(popularTeas);
 console.log(softCopyTree);
+console.log("-------------------------------------------------");
 
 // Question 7
 
@@ -43,6 +53,7 @@ let hardCopyCities = [...topCities]; ///creating hardcopy of an array
 topCities.pop();
 console.log(topCities);
 console.log(hardCopyCities);
+console.log("-------------------------------------------------");
 
 //Question 8
 
@@ -50,11 +61,13 @@ let europeanCitis = ["Paris", "Rome"];
 let asianCities = ["Tokyo", "Bangkok"];
 let worldCities = europeanCitis.concat(asianCities);
 console.log(worldCities);
+console.log("-------------------------------------------------");
 
 //Question 9
 
 let teaMenu = ["masala tea", "oolong tea", "green tea", "earl tea"];
 let menuLength = teaMenu.length;
 console.log(menuLength);
+console.log("-------------------------------------------------");
 
 //Question 10
