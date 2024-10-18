ae95364d03128a4a4d67152445a043fa092141cb
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2024-10-18 18:32:05 +0530

    solved question 1, 3 and 4

--- arrayChallenges.js
+++ arrayChallenges.js
@@ -1,1 +1,19 @@
 console.clear();
+
+//Question 1
+
+let teaFlavours = ["green tea", "black tea", "oolong tea"];
+let firstTea = teaFlavours[0];
+console.log(firstTea);
+
+// Question 3
+
+let teaTypes = ["herbal tea", "white tea", "masala tea"];
+teaTypes[1] = "jasmine tea";
+console.log(teaTypes);
+
+//Question 4
+
+let citiesVisited = ["Mumbai", "Sydney"];
+citiesVisited.push("Berlin");
+console.log(citiesVisited);
